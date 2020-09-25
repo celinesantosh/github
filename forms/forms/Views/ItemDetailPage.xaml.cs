d896f263a34e71773f5ffd8213bec0780151f47b
celinesantosh@gmail.com <celinesantosh@gmail.com>
2020-09-25 16:14:05 +0530

    importing project

---
+++ forms/forms/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using forms.ViewModels;
+
+namespace forms.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
